554bd78d955c26974c208babe3306cdd87aec106
Julian Salazar <jusalazard@unal.edu.co>
2024-03-12 00:13:54 -0500

    box

--- fracture/fracture.py
+++ fracture/fracture.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
-import vtk
 
 
 class Fracture:
@@ -15,7 +14,8 @@ class Fracture:
         self.axis = None
         self.fig = None
 
-    def load(self, filename):
+    @staticmethod
+    def load(filename):
 
         dataset = []
         with open(filename, 'r') as file:
@@ -26,7 +26,7 @@ class Fracture:
 
     def load_matrix(self):
 
-        self.M = self.load("FRAC0006_nrIter27.txt")
+        self.M = Fracture.load("FRAC0006_nrIter27.txt")
         self.A = np.dot(self.M.T, self.M)
 
         self.mp = np.mean(self.M, axis=0)
@@ -56,66 +56,50 @@ class Fracture:
 
     def print_plotly(self):
         self.fig = go.Figure()
-        # Agregar los autovectores a la figura
-        ''' 
-        for i in range(len(self.autovalues)):
-            autovector = self.autovectors[:, i]
-            x = autovector[0] / np.linalg.norm(autovector[0])
-            y = autovector[1] / np.linalg.norm(autovector[1])
-            z = autovector[2] / np.linalg.norm(autovector[2])
 
-            self.fig.add_trace(go.Scatter3d(
-                x=[self.mp[0], self.mp[0] + x],
-                y=[self.mp[1], self.mp[1] + y],
-                z=[self.mp[2], self.mp[2] + z],
-                mode='lines',
-                line=dict(color='red'),
-                name=f'Autovector {i + 1}'
-            ))
-            '''
+        max_p = np.amax((self.M - self.mp) @ self.autovectors, axis=0)
+        min_p = np.amin((self.M - self.mp) @ self.autovectors, axis=0)
 
-        max_extremos = np.amax((self.M - self.mp) @ self.autovectors, axis=0)
-        min_extremos = np.amin((self.M - self.mp) @ self.autovectors, axis=0)
-
-        puntos_caja = []
+        # crear vertices y drireccion de la caja
+        box = []
         for i in range(2):
             for j in range(2):
                 for k in range(2):
-                    punto = np.array([max_extremos[0] if i else min_extremos[0],
-                                      max_extremos[1] if j else min_extremos[1],
-                                      max_extremos[2] if k else min_extremos[2]])
+                    points = np.array([max_p[0] if i else min_p[0],
+                                       max_p[1] if j else min_p[1],
+                                       max_p[2] if k else min_p[2]])
                     # Transformar el punto de vuelta al espacio original
-                    puntos_caja.append(self.mp + punto @ self.autovectors.T)
+                    box.append(self.mp + points @ self.autovectors.T)
 
-        vertices = [
-            [puntos_caja[0], puntos_caja[1], puntos_caja[3], puntos_caja[2], puntos_caja[0]],
-            [puntos_caja[4], puntos_caja[5], puntos_caja[7], puntos_caja[6], puntos_caja[4]],
-            [puntos_caja[0], puntos_caja[1], puntos_caja[5], puntos_caja[4], puntos_caja[0]],
-            [puntos_caja[2], puntos_caja[3], puntos_caja[7], puntos_caja[6], puntos_caja[2]],
-            [puntos_caja[1], puntos_caja[3], puntos_caja[7], puntos_caja[5], puntos_caja[1]],
-            [puntos_caja[0], puntos_caja[2], puntos_caja[6], puntos_caja[4], puntos_caja[0]]
+        vertex = [
+            [box[0], box[1], box[3], box[2], box[0]],
+            [box[4], box[5], box[7], box[6], box[4]],
+            [box[0], box[1], box[5], box[4], box[0]],
+            [box[2], box[3], box[7], box[6], box[2]],
+            [box[1], box[3], box[7], box[5], box[1]],
+            [box[0], box[2], box[6], box[4], box[0]]
         ]
 
         # Crear trazas para cada cara de la caja
-        trazas = []
-        for vertice in vertices:
+        traces = []
+        for vertice in vertex:
             x, y, z = zip(*vertice)
-            traza = go.Scatter3d(
+            trace = go.Scatter3d(
                 x=x,
                 y=y,
                 z=z,
                 mode='lines',
                 line=dict(width=2)
             )
-            trazas.append(traza)
+            traces.append(trace)
 
-        # Crear figura y agregar trazas
-        self.fig = go.Figure(data=trazas)
+        self.fig = go.Figure(data=traces)
 
         # Agregar puntos a la figura
         self.fig.add_trace(go.Scatter3d(x=self.M[:, 0], y=self.M[:, 1], z=self.M[:, 2], mode='markers',
-                                        marker=dict(color='blue', size=5)))
+                                        marker=dict(color='blue', size=2), name='points'))
 
+        # graficar autovectores
         for i in range(len(self.autovalues)):
             autovector = self.autovectors[:, i]
             punto_inicio = self.mp - autovector
@@ -129,34 +113,5 @@ class Fracture:
                 name=f'Autovector {i + 1}'
             ))
 
-
         # Mostrar figura
         self.fig.show()
-        '''
-         x_min, y_min, z_min = np.min(self.M, axis=0)
-        x_max, y_max, z_max = np.max(self.M, axis=0)
-        box = [
-            # Cara frontal
-            [(x_min, y_min, z_min), (x_max, y_min, z_min), (x_max, y_max, z_min), (x_min, y_max, z_min)],
-            # Cara trasera
-            [(x_min, y_min, z_max), (x_max, y_min, z_max), (x_max, y_max, z_max), (x_min, y_max, z_max)],
-            # Conectar las caras
-            [(x_min, y_min, z_min), (x_min, y_min, z_max)],
-            [(x_max, y_min, z_min), (x_max, y_min, z_max)],
-            [(x_max, y_max, z_min), (x_max, y_max, z_max)],
-            [(x_min, y_max, z_min), (x_min, y_max, z_max)]
-        ]
-
-        for face in box:
-            x_face = [p[0] for p in face]
-            y_face = [p[1] for p in face]
-            z_face = [p[2] for p in face]
-            self.fig.add_trace(
-                go.Scatter3d(x=x_face + [x_face[0]], y=y_face + [y_face[0]], z=z_face + [z_face[0]], mode='lines'))
-
-        # Configurar el diseño de la figura
-        # self.fig.update_layout(scene=dict(aspectmode='cube'))
-
-        # Mostrar figura
-        self.fig.show()
-        '''
